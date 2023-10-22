76bde7a1214a7133780e84091ee2b8b052b0e060
Sanjeeth Tharmarajah <130941252+SanjeethTharmarajah@users.noreply.github.com>
2023-10-21 22:02:42 -0400

    Update Resume.jsx

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -19,7 +19,7 @@ export default function Resume() {
         <div className='col'>
             <div className='card'>
                 <div className="header">
-                    <h3>Download Resume: <a href={resume_pdf}>Sanjeeth.T</a>
+                    <h3>Download Resume: <a href={resume_pdf} download>Sanjeeth.T</a>
                     </h3>
                 </div>
                 
